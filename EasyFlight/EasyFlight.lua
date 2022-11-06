d61cbccdc8b6cb652828122d33cb5a7eff22d738
MindScape00 <MindScape00@users.noreply.github.com>
2022-11-05 21:58:10 -0700

    add landing back, add shift requirement

--- EasyFlight/EasyFlight.lua
+++ EasyFlight/EasyFlight.lua
@@ -1,34 +1,39 @@
-local self = CreateFrame("FRAME", "EasyFlight")
+local self = CreateFrame("FRAME")
 
-self.timer = 0
+self.flyTimer = 0
+self.landTimer = 0
 self.isFlying = false
 self.isSprint = false
 self.isSpacePressed = false
 
 hooksecurefunc("JumpOrAscendStart", function()
-	if IsFalling() then
+	if IsFalling() and IsShiftKeyDown() then
 		SendChatMessage(".cheat fly on", "GUILD")
 		self.isFlying = true
 		return
 	end
 	self.isSpacePressed = true
 end)
-self:RegisterEvent("OnUpdate")
 
 self:SetScript("OnUpdate", function(self, elapsed)
-	if self.timer < .5 then
-		self.timer = self.timer + elapsed
+	if self.flyTimer < .5 then
+		self.flyTimer = self.flyTimer + elapsed
 	end
 	if IsFlying() and self.isSpacePressed then
-		if self.timer < .5 then
+		if self.flyTimer < .5 then
 			SendChatMessage(".cheat fly off", "GUILD")
 			self.isFlying = false
 		end
 		self.isSpacePressed = false
-		self.timer = 0
+		self.flyTimer = 0
 	end
-	--if not IsFlying() and not IsFalling() and self.isFlying then
-	--	SendChatMessage(".cheat land")
-	--	self.isFlying = false
-	--end
+	if not IsFlying() and not IsFalling() and self.isFlying then
+		if self.landTimer > 1 then
+			SendChatMessage(".cheat fly off", "GUILD")
+			self.isFlying = false
+		end
+		self.landTimer = self.landTimer + elapsed
+	else
+		self.landTimer = 0
+	end
 end)
